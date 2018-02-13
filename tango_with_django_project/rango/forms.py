2cf9abcdfd9036570b668eb41a7336a84c079e16
Nicholas Ferrara <nferrara100@gmail.com>
2018-02-13 15:39:11 +0000

    Chapter 7

---
+++ tango_with_django_project/rango/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from rango.models import Page, Category
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(
+        max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # An inline class to provide additional information on the forms
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Category
+        fields = ('name',)
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(
+        max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://',  then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
+
+    class Meta:
+        # Again Provide an association between ModelForm and the model
+        model = Page
+
+        # Can use this inline class to hide the foreign key, which is the Category
+        exclude = ('category',)
+        # could also specify the fields to include, ie. just not include category field
+        #fields = ('title', 'url', 'views')
