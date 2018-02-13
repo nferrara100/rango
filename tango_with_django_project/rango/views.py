0824352b75497be5a07e463ef9b6477296ecf366
Nicholas Ferrara <nferrara100@gmail.com>
2018-02-13 17:21:19 +0000

    Chapter 10

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,27 +8,59 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(
+        request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(
+        last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 
 def index(request):
     #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
-
     page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {'categories': category_list, 'pages': page_list}
-    for c in context_dict.items():
-        print(c)
 
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    print(request.session['visits'])
     response = render(request, 'rango/index.html', context=context_dict)
-
     return response
 
 
 def about(request):
-    print(request.method)
-    print(request.user)
-    return render(request, 'rango/about.html', {})
+    context_dict = {}
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    print(request.session['visits'])
+
+    return render(request, 'rango/about.html', context=context_dict)
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>View Index Page</a>")
 
 
